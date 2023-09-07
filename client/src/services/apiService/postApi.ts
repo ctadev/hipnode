1f4b4d563c20603999451a59c470bf8b2e58f8f0
Benjamin Huy Pham <benjaminhpham@gmail.com>
2023-09-06 22:48:38 -0700

    refactored apiService calls

--- client/src/services/apiService/postApi.ts
+++ client/src/services/apiService/postApi.ts
@@ -1,8 +1,22 @@
-import axios from 'axios';
+import axios, { AxiosError } from 'axios';
+import { IPost } from '../../../types/index';
 
 const API_URL = import.meta.env.VITE_DEV_BACKEND_URL;
 
-export const fetchPosts = async () => {
-  const res = await axios(`${API_URL}/posts`);
-  return await res.data;
+interface IApiError {
+  message: string;
+}
+
+const handleError = (err: AxiosError<IApiError>): never => {
+  throw new Error(err.response?.data.message);
+};
+
+export const fetchPosts = async (): Promise<IPost[]> => {
+  try {
+    const res = await axios.get<IPost[]>(`${API_URL}/posts`);
+    return res.data;
+  } catch (err) {
+    handleError(err as AxiosError<IApiError>);
+    return [];
+  }
 };
--- client/src/services/apiService/userApi.ts
+++ client/src/services/apiService/userApi.ts
@@ -1,4 +1,5 @@
-import axios from 'axios';
+import axios, { AxiosError } from 'axios';
+import { IUser } from '../../../types/index';
 
 interface ILoginUser {
   username?: string;
@@ -8,26 +9,29 @@ interface ILoginUser {
 
 const API_URL = import.meta.env.VITE_DEV_BACKEND_URL;
 
-const handleError = (err: any) => {
-  if (err && err.response) {
-    const messageData = err.response?.message?.data;
-    throw new Error(messageData ? messageData : err.message);
-  }
-  throw new Error('An error occured');
+interface IApiError {
+  message: string;
+}
+
+const handleError = (err: AxiosError<IApiError>): never => {
+  throw new Error(err.response?.data.message);
 };
 
-export const getUserInfo = async (id: number) => {
-  const res = await axios.get(`${API_URL}/users/${id}/profile`);
-  return await res.data;
+export const getUserInfo = async (id: number): Promise<IUser> => {
+  try {
+    const res = await axios.get<IUser>(`${API_URL}/users/${id}/profile`);
+    return res.data;
+  } catch (err) {
+    handleError(err as AxiosError<IApiError>);
+  }
 };
 
 export const loginUser = async (user: ILoginUser) => {
   try {
     const res = await axios.post(`${API_URL}/users/login`, user);
-    console.log('response', res);
     return res.data;
-  } catch (err: any) {
-    handleError(err);
+  } catch (err) {
+    handleError(err as AxiosError<IApiError>);
   }
 };
 
@@ -35,7 +39,7 @@ export const registerUser = async (user: ILoginUser) => {
   try {
     const res = await axios.post(`${API_URL}/users/register`, user);
     return res.data;
-  } catch (err: any) {
-    handleError(err);
+  } catch (err) {
+    handleError(err as AxiosError<IApiError>);
   }
 };
